c8064b243d88507eb20f08655b61944d220d4a86
unknown <hafsabinteharun101@gmail.com>
2024-01-13 20:04:27 +0600

    first

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY= 'django-insecure-)o@4k56uuob$z5kw6ulan^5k-39$euveiyiqicbsv(hanj20i4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
